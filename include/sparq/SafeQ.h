9d603c66f4ae6243970f2a4aa12b4bcdb41ea09e
Thomas Benson <thomas.benson@smiths.com>
2018-11-30 15:27:30 -0500

    [SMPUM-3531] Switch from cv::wait_until to pthread_cond_timedwait
    Switch to the POSIX pthread API so that we can use a monotonic
    clock for waits.

--- include/sparq/SafeQ.h
+++ include/sparq/SafeQ.h
@@ -10,64 +10,100 @@
 #include <queue>
 #include <mutex>
 #include <condition_variable>
+#include <chrono>
+#include <pthread.h>
 
 namespace sparq {
+    // SafeQ uses the POSIX pthread API so that it can wait based on a
+    // monotonic clock (and thus support wait times that do not change
+    // based on system time changes). The current implementation of
+    // std::condition_variable does not support this use case (see
+    // https://gcc.gnu.org/bugzilla/show_bug.cgi?id=41861). This is less
+    // portable than the C++ standard library (i.e. no Windows support),
+    // but avoids race conditions that could cause a timeout to be delayed
+    // by an arbitrary amount (or time out too soon).
     template <class T>
     class SafeQ {
     public:
-        SafeQ() : queue(), mutex(), cv() {}
+        SafeQ() : queue() {
+            pthread_mutex_init(&mutex, NULL);
+            pthread_condattr_init(&condattr);
+            pthread_condattr_setclock(&condattr, CLOCK_MONOTONIC);
+            pthread_cond_init(&cond, &condattr);
+        }
 
         void push(T t) {
-            std::lock_guard<std::mutex> guard(mutex);
+            pthread_mutex_lock(&mutex);
             queue.push(t);
-            cv.notify_one();
+            pthread_mutex_unlock(&mutex);
+            pthread_cond_signal(&cond);
         }
 
         T pop() {
-            std::unique_lock<std::mutex> lock(mutex);
-            while (queue.empty())  cv.wait(lock);
+            pthread_mutex_lock(&mutex);
+            while (queue.empty()) pthread_cond_wait(&cond, &mutex);
             T val = queue.front();
             queue.pop();
+            pthread_mutex_unlock(&mutex);
             return val;
         }
 
         bool tryPop(T* p) {
-            std::unique_lock<std::mutex> lock(mutex);
+            pthread_mutex_lock(&mutex);
             if (!queue.empty()) {
                 p[0] = queue.front();
                 queue.pop();
+                pthread_mutex_unlock(&mutex);
                 return true;
             }
-            cv.wait(lock);
-            if (queue.empty()) return false;
+            pthread_cond_wait(&cond, &mutex);
+            if (queue.empty()) {
+                pthread_mutex_unlock(&mutex);
+                return false;
+            }
             p[0] = queue.front();
             queue.pop();
+            pthread_mutex_unlock(&mutex);
             return true;
         }
 
         template <class S>
         bool tryPopUntil(T* p, const S& when) {
-            std::unique_lock<std::mutex> lock(mutex);
+            pthread_mutex_lock(&mutex);
             if (!queue.empty()) {
                 p[0] = queue.front();
                 queue.pop();
+                pthread_mutex_unlock(&mutex);
                 return true;
             }
-            cv.wait_until(lock,when);
-            if (queue.empty()) return false;
+
+            const auto secs = std::chrono::time_point_cast<std::chrono::seconds>(when);
+            const auto nsecs = std::chrono::duration_cast<std::chrono::nanoseconds>(when - secs);
+            const struct timespec ts = {
+                static_cast<std::time_t>(secs.time_since_epoch().count()),
+                static_cast<long>(nsecs.count())
+            };
+
+            pthread_cond_timedwait(&cond, &mutex, &ts);
+
+            if (queue.empty()) {
+                pthread_mutex_unlock(&mutex);
+                return false;
+            }
             p[0] = queue.front();
             queue.pop();
+            pthread_mutex_unlock(&mutex);
             return true;
         }
 
         void signal() {
-            std::lock_guard<std::mutex> guard(mutex);
-            cv.notify_one();
+            pthread_cond_signal(&cond);
         }
     private:
         std::queue<T> queue;
-        std::mutex mutex;
-        std::condition_variable cv;
+        pthread_mutex_t mutex;
+        pthread_cond_t cond;
+        pthread_condattr_t condattr;
     };
 }
 
